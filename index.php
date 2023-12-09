898661704c3bf705d6f91250c463dcb3250c486f
Shaik Tabrez <37013844+tabrezdn1@users.noreply.github.com>
2023-12-09 15:58:26 -0500

    restore

--- index.php
+++ index.php
@@ -83,10 +83,10 @@
        		     <option value="Coffee">Coffee</option>
         			<option value="Soda">Soda</option>
     				</select>
-
+            <div>
             <label>Quantity</label>
             <input type="number" name="food-item-q">
-    				
+            </div>
         <button type="submit" class="register-btn" name="purchase">PURCHASE</button>
       </form>
     </div>
@@ -100,12 +100,23 @@
           <button type="reset" onclick="popup('register-popup')">X</button>
         </h2>
         <input type="text" placeholder="Full Name" name="fullname">
-        <input type="text" placeholder="Username" name="username">
         <input type="email" placeholder="E-mail" name="email">
-        <input type="text" placeholder="Address" name="address">
+        <label for="ticket-type">Type:</label>
+    				<select name="ticket-type" id="ticket-type">
+        			<option value="Admit">Zoo Admission</option>
+        			<option value="show1">show1</option>
+       		    <option value="show2">show2</option>
+        			<option value="show3">show3</option>
+    				</select>
+            <div>
+            <label for="ticket-for">Ticket for:</label>
+    				<select name="ticket-for" id="ticket-for">
+        			<option value="Adult">Adult</option>
+        			<option value="Child">Child</option>
+       		    <option value="Senior">Senior</option>
+    				</select>
+            </div>
 	    <input type="text" placeholder="Phone Number" name="phone">
-		<input type="text" placeholder="Credit Card Number" name="credit">
-	   <input type="password" placeholder="Password" name="password">
         <button type="submit" class="register-btn" name="register">BUY TICKETS</button>
       </form>
     </div>
