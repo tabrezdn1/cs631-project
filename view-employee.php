98b6fc0ffd4fffa78feb4735e8e9293295addc02
Shaik Tabrez <37013844+tabrezdn1@users.noreply.github.com>
2023-12-12 01:48:02 -0500

    fix view emp

--- view-employee.php
+++ view-employee.php
@@ -1,7 +1,7 @@
 <?php
 include 'connection.php';
 
-  $sql =  "SELECT * FROM employees as E, hourly_rate as H, revenue_types as R where E.hourly_rate_id = H.hourly_rate_id AND E.revenue_id=R.revenue_id";
+  $sql =  "SELECT * FROM employees as E, hourly_rate as H where E.hourly_rate_id = H.hourly_rate_id";
   $result = mysqli_query($conn, $sql);
 
 
@@ -46,7 +46,7 @@ include 'connection.php';
   <?php
   // Display table of appointments
   if (mysqli_num_rows($result) > 0) {
-    echo "<table><tr><th>Employee ID</th><th>Supervisor ID</th><th>First Name</th><th>Middle Name</th><th>Last Name</th><th>Join Date</th><th>Street</th><th>City</th><th>State</th><th>Zip</th><th>Job Type</th><th>Hourly Rate</th></tr>";
+    echo "<table><tr><th>First Name</th><th>Middle Name</th><th>Last Name</th><th>Join Date</th><th>Street</th><th>City</th><th>State</th><th>Zip</th><th>Job Type</th><th>Hourly Rate</th></tr>";
     while($row = mysqli_fetch_assoc($result)) {
      
       $emp_id = $row['emp_id'];
@@ -60,11 +60,11 @@ include 'connection.php';
       $state = $row['state'];
       $zip = $row['zip'];
       
-      $name =$row['name'];
+      $job_type =$row['job_type'];
       $rate = $row['rate'];
       
        
-        echo "<tr><td>$emp_id</td><td>$sup_id</td><td>$first_name</td><td>$middle_name</td><td>$last_name</td><td>$start_date</td><td>$street</td><td>$city</td><td>$state</td><td>$zip</td><td>$name</td><td>$rate</td></tr>";
+        echo "<tr><td>$first_name</td><td>$middle_name</td><td>$last_name</td><td>$start_date</td><td>$street</td><td>$city</td><td>$state</td><td>$zip</td><td>$job_type</td><td>$rate</td></tr>";
       
       
     }
